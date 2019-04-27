fba95f6a831ef8c0c9feb90e2b2966ea98e20eea
Markus Sitzmann <markus.sitzmann@gmail.com>
2019-04-28 00:43:10 +0200

    item subpage added; model improvements; first action button implementation (works for banner)

--- appsite/home/models.py
+++ appsite/home/models.py
@@ -1,17 +1,46 @@
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.db import models
-from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel, MultiFieldPanel, PageChooserPanel
+from modelcluster.fields import ParentalKey
+from wagtail.admin.edit_handlers import FieldPanel, StreamFieldPanel, MultiFieldPanel, PageChooserPanel, InlinePanel
 from wagtail.core.fields import StreamField, RichTextField
 
-from wagtail.core.models import Page
+from wagtail.core.models import Page, Orderable
 from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.search import index
+from wagtail.snippets.edit_handlers import SnippetChooserPanel
 from wagtail.snippets.models import register_snippet
 
 from base.blocks import BaseStreamBlock
 
 from home.sitedefaults import style_choices, orientation_choices, content_align_choices, image_position_choices, \
     onload_fade_choices, onscroll_fade_choices, color_choices
+
+
+@register_snippet
+class ActionButton(models.Model):
+    SIZE_CHOICES = (
+        ('', 'Default'),
+        ('small', 'Small'),
+    )
+
+    name = models.CharField(max_length=255)
+    url = models.URLField(max_length=255, blank=True, default='')
+    size = models.CharField(max_length=25, choices=SIZE_CHOICES, blank=True, default='')
+    icon = models.CharField(max_length=25, blank=True, default='')
+
+    panels = [
+        FieldPanel('name'),
+        FieldPanel('size'),
+        FieldPanel('url'),
+        FieldPanel('icon'),
+    ]
+
+    def __str__(self):
+        button_string = self.name + " (" + self.url + ")"
+        if self.size:
+            button_string = button_string + ", " + self.size
+        return button_string
+
 
 
 @register_snippet
@@ -115,14 +144,32 @@ class BannerPage(Page):
         FieldPanel('minor'),
         ImageChooserPanel('image'),
         FieldPanel('options'),
+        InlinePanel('actions', label="Actions"),
     ]
 
     parent_page_types = ['HomePage']
+    subpage_types = []
 
     def get_context(self, request):
         context = super(BannerPage, self).get_context(request)
         context['banner'] = self
         return context
+
+
+class BannerPageActionButtons(Orderable, models.Model):
+    page = ParentalKey('home.BannerPage', on_delete=models.CASCADE, related_name='actions')
+    button = models.ForeignKey('home.ActionButton', on_delete=models.CASCADE, related_name='+')
+
+    class Meta:
+        verbose_name = "action button"
+        verbose_name_plural = "action buttons"
+
+    panels = [
+        SnippetChooserPanel('button'),
+    ]
+
+    def __str__(self):
+        return self.page.title + " -> " + self.button.name
 
 
 class SectionPage(Page):
@@ -142,6 +189,11 @@ class SectionPage(Page):
     ]
 
     parent_page_types = ['SectionIndexPage']
+
+    def get_context(self, request):
+        context = super(SectionPage, self).get_context(request)
+        context['section'] = self
+        return context
 
 
 class SectionIndexPage(Page):
@@ -217,7 +269,13 @@ class SpotlightPage(Page):
         index.SearchField('content'),
     ]
 
+    subpage_types = []
     parent_page_types = ['SpotlightIndexPage']
+
+    def get_context(self, request):
+        context = super(SpotlightPage, self).get_context(request)
+        context['spotlight'] = self
+        return context
 
 
 class SpotlightIndexPage(Page):
@@ -286,7 +344,13 @@ class GalleryArticlePage(Page):
         index.SearchField('text'),
     ]
 
+    subpage_types = []
     parent_page_types = ['GalleryPage']
+
+    def get_context(self, request):
+        context = super(GalleryArticlePage, self).get_context(request)
+        context['gallery_article'] = self
+        return context
 
 
 class GalleryPage(Page):
@@ -328,8 +392,81 @@ class GalleryPage(Page):
     def get_context(self, request):
         context = super(GalleryPage, self).get_context(request)
         articles = self.paginate(request, self.get_articles())
-        context['gallery'] = self
         context['articles'] = articles
+        context['gallery'] = self
+        return context
+
+
+class ItemPage(Page):
+    """"""
+    headline = models.CharField(
+        null=True,
+        blank=True,
+        max_length=255,
+    )
+    text = models.TextField(
+        help_text='Text to describe the page',
+        blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('headline'),
+        FieldPanel('text'),
+    ]
+
+    search_fields = Page.search_fields + [
+        index.SearchField('text'),
+    ]
+
+    subpage_types = []
+    parent_page_types = ['ItemIndexPage']
+
+    def get_context(self, request):
+        context = super(ItemPage, self).get_context(request)
+        context['item'] = self
+        return context
+
+
+class ItemIndexPage(Page):
+    """"""
+    headline = models.CharField(
+        null=True,
+        blank=True,
+        max_length=255,
+    )
+    introduction = models.TextField(
+        help_text='Text to describe the page',
+        blank=True)
+
+    content_panels = Page.content_panels + [
+        FieldPanel('headline', classname="full"),
+        FieldPanel('introduction', classname="full"),
+    ]
+
+    parent_page_types = []
+    subpage_types = ['ItemPage']
+
+    def get_items(self):
+        return ItemPage.objects.live().descendant_of(self)
+
+    def children(self):
+        return self.get_children().specific().live()
+
+    def paginate(self, request, *args):
+        page = request.GET.get('page')
+        paginator = Paginator(self.get_items(), 12)
+        try:
+            pages = paginator.page(page)
+        except PageNotAnInteger:
+            pages = paginator.page(1)
+        except EmptyPage:
+            pages = paginator.page(paginator.num_pages)
+        return pages
+
+    def get_context(self, request):
+        context = super(ItemIndexPage, self).get_context(request)
+        items = self.paginate(request, self.get_items())
+        context['items'] = items
+        context['item_index'] = self
         return context
 
 
@@ -355,7 +492,7 @@ class HomePage(Page):
         PageChooserPanel('section_1'),
     ]
 
-    subpage_types = ['BannerPage', 'SpotlightIndexPage', 'SectionIndexPage', 'GalleryPage']
+    subpage_types = ['BannerPage', 'GalleryPage', 'ItemIndexPage', 'SpotlightIndexPage', 'SectionIndexPage']
 
     def __str__(self):
         return self.headline
